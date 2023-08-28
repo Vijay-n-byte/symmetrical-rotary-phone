98af0f99558b023c8c6e7e64c8fb4f4ae8763e79
vijay <vd661163@gmail.com>
2023-08-28 16:52:24 +0530

    seventh new commit

--- src/app/quiz/quiz/quiz.component.ts
+++ src/app/quiz/quiz/quiz.component.ts
@@ -5,7 +5,7 @@ import { NgForm,FormsModule} from '@angular/forms';
 import { AppRoutingModule } from 'src/app/app-routing.module';
 import { Router ,ActivatedRoute} from '@angular/router';
 import { HomepageComponent } from 'src/app/homepage/homepage.component';
-import {interval,take} from 'rxjs';
+import {Subject, interval,take, takeUntil} from 'rxjs';
 
 @Component({
   selector: 'app-miss',
@@ -13,10 +13,19 @@ import {interval,take} from 'rxjs';
   styleUrls: ['./quiz.component.css'],
   encapsulation:ViewEncapsulation.Emulated
 })
-export class QuizComponent implements OnInit{
+export class QuizComponent implements OnInit,OnDestroy{
   sample:string|null='';
-  //y:number=0;
   y:number=60;
+  timee=interval(1000).subscribe(n=>{
+    this.y--;
+    if(this.y===0){
+      this.b.setusersanswers(this.users2);
+      localStorage.setItem("review","1");
+      this.c.navigate(['../review'],{relativeTo:this.route});
+    }
+  } 
+    );
+
   public users1:Array<Qns>=[];//nnn-it is for questions array
   public users2:Array<number>=[];//nnn-it saves answers
   constructor(private b:DataserviceService,
@@ -26,19 +35,11 @@ export class QuizComponent implements OnInit{
     this.sample=localStorage.getItem("name");
     this.b.getsinglequestions(1).subscribe(j=>this.users1=j);
     localStorage.setItem("test","0");//now
-    const timee=interval(1000).pipe(take(60));
-    timee.subscribe(n=>{
-      this.y--;
-      if(this.y===0){
-        this.b.setusersanswers(this.users2);
-        localStorage.setItem("review","1");
-        
-        this.c.navigate(['../review'],{relativeTo:this.route});
-      }
-    } 
-      );
   }
-  
+  ngOnDestroy(): void {
+    this.timee.unsubscribe();
+  }
+
   submitfinal(f:NgForm){
     let aa:number=f.value.r;
     this.users2.push(aa);
